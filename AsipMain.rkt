d8079b430970b3eaff8d4e63c7ae49fa7ad9fe32
Franco <f.raimondi@gmail.com>
2014-09-10 21:50:13 +0100

    Initial structure for ASIP client with in and out

---
+++ AsipMain.rkt
@@ -0,0 +1,242 @@
+#lang racket
+
+;; ****************************
+;; **** RACKET ASIP CLIENT ****
+;; ****************************
+
+;; Authors: Franco Raimondi and Micky Silas
+;; For information regarding the ASIP protocol, please
+;; see https://github.com/michaelmargolis/asip
+
+;; The basic idea is similar to racket-firmata: we set up input and output 
+;; streams attached to a serial port. The main thread (this one) deals with
+;; writing, a separate thread handles the input. The input thread writes 
+;; values to arrays of bit values, see below.
+
+;; TODO [FR]: Here we may want to export something, commented for the moment.
+;; (provide is-pin-set?,
+;;          is-arduino-pin-set?,
+;;          etc...
+;; )
+
+
+; bit-operations
+(require file/sha1)
+; Needed for setting the terminal baudrate
+(require racket/system)
+;; Needed to detect the port.
+(require "AsipUtilities.rkt") 
+
+;; These are the input and output ports and the thread handling input messages.
+(define in                null) 
+(define out               null)
+(define read-thread       null)
+
+
+
+;; *** BEGIN SECTION TO DEFINE SERIAL CONNECTION ***
+;; This function creates in and out and sets the read thread.
+;; Copied from racket-firmata.rkt
+(define BAUDRATE           57600)
+
+(define (open-asip) 
+  (define port-name (get-port))
+  ;; We set the command line instruction to configure the serial port according to the OS;
+  ;; we also configure the file name of the port to be opened (it is different in win)
+  (define call-string null)
+  (define filename null)
+  (define os (detect-os))
+  (cond 
+   ( (equal? os "linux") 
+     (set! call-string (string-append  "stty -F " port-name " cs8 " BAUDRATE " ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts"))
+     (set! filename port-name))        
+   ( (equal? os "mac") 
+     (set! call-string (string-append  "stty -f " port-name BAUDRATE " cs8 cread clocal"))
+     (set! filename port-name))
+   ( (equal? os "win") 
+     (set! call-string (string-append  "mode " port-name ": baud=" BAUDRATE " parity=N data=8 stop=1"))
+     (set! filename (string-append "\\\\.\\" port-name)))
+   ) ;; end of cond to set stty or mode string and filename.
+
+  (cond ( (equal? os "win") 
+	  (if (system call-string) ;; here we set the port
+	      (begin
+	       (let-values ([(in-port out-port) (open-input-output-file filename)])
+			   (set! in in-port)
+			   (set! out out-port)
+			   (file-stream-buffer-mode out 'none)
+			   )
+	       (sleep 3)
+	       (set! read-thread (thread (lambda ()  (read-hook)))) ;; we set the reading thread
+	       #t)
+	    (error "Failed to open the connection with " port-name " verify if your microcontroller is plugged in correctly"))            
+	  )
+        (else   
+         (set! out (open-output-file port-name #:mode 'binary #:exists 'append))
+         (set! in  (open-input-file  port-name #:mode 'binary))
+         (file-stream-buffer-mode out 'none)
+         (sleep 2)
+         (if (system call-string) ;; here we set the port
+             (begin
+	      (sleep 1)
+	      (set! read-thread (thread (lambda ()  (read-hook)))) ;; we set the reading thread
+	      (printf "Success opening the serial port\n")
+	      #t)
+	   (error "Failed to open the connection with " port-name " verify if your microcontroller is plugged in correctly"))            
+	 )               
+	)
+  ) ;; end of open-asip
+;; *** END SECTION TO SET UP SERIAL CONNECTION ***
+
+
+;; *** ASIP GENERIC CONSTANTS ***
+(define EVENT_HANDLER           "@")
+(define ERROR_MESSAGE_HEADER     "~")
+(define DEBUG_MESSAGE_HEADER     "!")
+
+;; Usually followed by a time interval in milliseconds to set autoevent status 
+;; (time=0 means disable autoevents)
+(define AUTOEVENT_MESSAGE        "A")
+
+;; Used to re-map pins
+(define REMAP_PIN_MESSAGE        "M")
+
+;; A standard event (such as reporting distance, etc.)
+(define ASIP_EVENT               "e")
+;; END OF ASIP GENERIC CONSTANTS
+
+
+;; *** DEFINITION OF ASIP CONSTANTS FOR I/O SERVICE ***
+(define IO_SERVICE              "I")
+(define PIN_MODE                "P")
+(define DIGITAL_WRITE           "d")
+(define ANALOG_WRITE            "a")
+(define PORT_DATA               "d")
+(define ANALOG_VALUE            "a")
+(define PORT_MAPPING            "M")
+
+;; Pin modes
+(define UNKNOWN_MODE             0)
+(define INPUT_MODE               1)
+(define INPUT_PULLUP_MODE        2)
+(define OUTPUT_MODE              3)
+(define ANALOG_MODE              4)
+(define PWM_MODE                 5)
+(define RESERVED_MODE            6)
+(define OTHER_SERVICE_MODE       7)
+
+(define HIGH                     1)
+(define LOW                      0)
+;; *** END ASIP CONSTANTS FOR I/O SERVICE ***
+
+
+;; *** We store digital and analog pins in fixed-length array.
+;; FIXME: this could be improved in the future, building the arrays after
+;; querying the board capabilities.
+(define MAX_NUM_DIGITAL_PINS  72) 
+(define MAX_NUM_ANALOG_PINS   16) 
+
+(define ANALOG-IO-PINS (make-vector MAX_NUM_ANALOG_PINS))
+(define DIGITAL-IO-PINS (make-vector MAX_NUM_DIGITAL_PINS))
+
+
+;; *** DEFINTIONS TO WRITE MESSAGES **TO** ARDUINO ***
+
+;; Setting a pin to a certain mode (INPUT, OUTPUT, PWM, etc.)
+(define (set-pin-mode pin mode)
+  (write (string-append IO_SERVICE "," PIN_MODE "," (number->string pin) "," 
+			(number->string mode) ) out)
+)
+
+;; Writing a value (high or low) to a digital pin
+(define (digital-write pin value)
+  (write (string-append IO_SERVICE "," DIGITAL_WRITE "," (number->string pin) 
+			"," (number->string value)) out)
+)
+
+(define (analog-write pin value)
+  (write (string-append IO_SERVICE "," ANALOG_WRITE "," (number->string pin) 
+			"," (number->string value)) out)
+)
+
+;; Set auto-reporting for I/O to a certain time in ms (needed for analog input pins)
+(define (set-autoreport timems)
+  (write (string-append IO_SERVICE "," AUTOEVENT_MESSAGE "," (number->string timems)) out)
+)
+
+;; Utility functions for compatibility with old Firmata code
+(define (set-arduino-pin! pin) (digital-write pin HIGH))
+(define (clear-arduino-pin! pin) (digital-write pin LOW))
+(define set-pin-mode! set-pin-mode)
+
+;; *** END OF FUNCTIONS TO WRITE TO ARDUINO ***
+
+
+;; *** FUNCTIONS TO HANDLE MESSAGES FROM ARDUINO AND TO READ VALUES ***
+(define (digital-read pin)
+  (vector-ref DIGITAL-IO-PINS pin))
+
+(define (analog-read pin)
+  (vector-ref ANALOG-IO-PINS pin))
+
+
+(define (read-hook)
+  (printf "Read thread started ...")
+  (read-loop))
+
+(define (read-loop)
+  ;; We read a whole line (ASIP messages are terminated with a \n
+  (process-input (read-line in))
+  (read-loop))
+
+(define (process-input input)
+  (let  ([char (substring input 0 1)])
+    (cond
+      [(equal? char EVENT_HANDLER)         (handle-input-event input)]
+      [(equal? char ERROR_MESSAGE_HEADER)  (handle-input-event input)]
+      [(equal? char DEBUG_MESSAGE_HEADER)  (handle-input-event input)])))
+
+  
+;(define (process-pin-mapping mapping)
+  
+  
+(define (handle-input-event input)
+  (let ([char (substring input 1 2)])
+    (cond 
+      [(equal? char IO_SERVICE)
+       (let ([service (substring input 3 4)])
+         (cond 
+           [(equal? service PORT_DATA)
+            (process-port-data input)]
+           [(equal? service PORT_MAPPING)
+            (process-pin-data input)]
+           [(equal? service ANALOG_VALUE)
+            (process-analog-values input)]))])))
+
+
+;; Placeholders. TODO: code them!!
+(define (process-port-data input) null)
+(define (process-analog-values) null)
+                  
+;; Processing port mapping is the most complicated part of ASIP. The initial message tells how to
+;; map port bits to pins. Example message FROM Arduino:
+;; - @I,M,20,{4:1,4:2,4:4,4:8,4:10,4:20,4:40,4:80,2:1,2:2,2:4,2:8,2:10,2:20,3:1,3:2,3:4,3:8,3:10,3:20}
+;; (this is the mapping of pins: pin 0 is mapped to the first bit of port
+;; 4, pin 1 to the second bit of port 4, etc. MAPPING IS IN HEX! so 20 is
+;; 32. Take the conjunction of this with the port and you get the pin
+;; value)
+(define (process-pin-data input) null )
+
+
+;; Find the index of something in a list (I couldn't find a function for this!)
+;; Copied from stackoverflow and slightly modified. str and x need to be string
+;; and we only look for the first character of x. Really horrible code...
+(define (str-index-of str x)
+  (define l (string->list str))
+  (for/or ([y l] [i (in-naturals)] #:when (equal? (string-ref x 0) y)) i))
+
+;; - @I,a,3,{0:320,1:340,2:329}
+;; (this are analog pins: 3 of them are set, analog pins 0, 1 and 2 in
+;; this case, and their values are in bracket).
+
+(str-index-of "PIPPO" "O")
---
+++ AsipUtilities.rkt
@@ -0,0 +1,56 @@
+#lang racket/base
+
+;; This file collects various utility functions taken from racket-firmata.rkt
+;; and developed mainly by Nikos. The functions are used to detect the OS and to
+;; find the serial port to which Arduino is attached. Only get-port is exported.
+
+(require racket/system)
+(require racket/list)
+(require racket/string)
+(require racket/match)
+
+(provide get-port
+	 detect-os)
+
+
+;; Operating system: can be linux, mac, win
+;; The os is needed to open the serial port in the appropriate way.
+(define (detect-os)
+  (define racket-os (system-type))
+  (cond
+    [(equal? racket-os 'windows) "win"]
+    [else 
+     (let ([machine (first (string-split (system-type 'machine)))])
+       (match machine
+         ["Darwin" "mac"]
+         ["Linux" "linux"]
+         [_ (raise "Unknown OS detected in firmata")] 
+         ))]))
+
+;; don't connect devices through USB that expose serial interfaces at the 
+;; same time as the arduino
+;; (such as mobile phones with tethering on)
+;; OR anything on the serial ports
+(define (find-port check-port prefix num)
+  (if (<= 0 num 255)
+      (let ([port (string-append prefix (number->string num))])
+        (if (check-port port) port (find-port check-port prefix (+ 1 num))))
+      #f))
+
+(define (find-mac-port)
+  (string-append "/dev/" (path->string (first (for/list ([f (directory-list "/dev")] #:when (regexp-match? "tty.usbmodem*" f))
+     f))))
+  )
+
+(define (valid-com-port port)
+  (system (string-append "mode " port " > NUL")))
+
+(define (get-port)
+  (define os (detect-os))
+  (match os
+    ("linux" (or (find-port file-exists? "/dev/ttyACM" 0)  (find-port file-exists? "/dev/ttyAMA" 0)))
+    ;; hopefully there is nothing attached above port 3... You may need to change this.
+    ("win" (find-port valid-com-port "COM" 3))
+    ("mac" (find-mac-port))
+    (_ (raise "Don't know how to find port on this OS"))
+    ))
